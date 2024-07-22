d06d6ae5f1b8755b0a9878b2cabaa25228e7c6c5
Yasas1994 <yasas.wijesekara@gmail.com>
2024-07-22 15:36:56 +0200

    code formatting & docstrings

--- src/jaegeraa/__init__.py
+++ src/jaegeraa/__init__.py
@@ -1,6 +1,4 @@
-import os 
+import os
 
-if not os.environ.get('TF_CPP_MIN_LOG_LEVEL',None):
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-
-
+if not os.environ.get("TF_CPP_MIN_LOG_LEVEL", None):
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
