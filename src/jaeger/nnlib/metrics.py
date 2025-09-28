52123e05014999d9c7aa141f3e7ff18b3ccc8069
Yasas1994 <yasas.wijesekara@gmail.com>
2025-09-29 01:49:07 +0200

    per class metrics

--- src/jaeger/nnlib/metrics.py
+++ src/jaeger/nnlib/metrics.py
@@ -1,85 +1,106 @@
 import tensorflow as tf
 
 
-class Precision_per_class(tf.keras.metrics.Metric):
-    def __init__(self, name="Precision_per_class", num_classes=4, **kwargs):
-        super(Precision_per_class, self).__init__(name=name, **kwargs)
-        self.true_positives = self.add_weight(
-            name="tp", initializer="zeros", shape=num_classes
-        )
-        self.pred_positives = self.add_weight(
-            name="pp", initializer="zeros", shape=num_classes
-        )
-        self.num_classes = num_classes
+class PrecisionForClass(tf.keras.metrics.Metric):
+    def __init__(self, class_id, name=None, **kwargs):
+        if name is None:
+            name = f"precision_class{class_id}"
+        super().__init__(name=name, **kwargs)
+        self.class_id = class_id
+        self.tp = self.add_weight(name="tp", initializer="zeros")
+        self.fp = self.add_weight(name="fp", initializer="zeros")
 
     def update_state(self, y_true, y_pred, sample_weight=None):
-        y_pred = tf.math.argmax(y_pred, axis=-1)
-        y_pred = tf.one_hot(y_pred, self.num_classes)
+        # Convert one-hot → integers if needed
+        if y_true.shape.rank > 1 and y_true.shape[-1] > 1:
+            y_true = tf.argmax(y_true, axis=-1)
+        y_pred = tf.argmax(y_pred, axis=-1)
 
-        y_true = tf.cast(y_true, tf.bool)
-        y_pred = tf.cast(y_pred, tf.bool)
+        y_true = tf.cast(y_true, tf.int32)
+        y_pred = tf.cast(y_pred, tf.int32)
 
-        tp = tf.logical_and(tf.equal(y_true, True), tf.equal(y_pred, True))
-        tp = tf.cast(tp, self.dtype)
-        pp = tf.equal(y_pred, True)
-        pp = tf.cast(pp, self.dtype)
+        y_true_i = tf.equal(y_true, self.class_id)
+        y_pred_i = tf.equal(y_pred, self.class_id)
 
-        if sample_weight is not None:
-            sample_weight = tf.cast(sample_weight, self.dtype)
-            sample_weight = tf.broadcast_to(sample_weight, self.shape)
-            tp = tf.multiply(tp, sample_weight)
-            pp = tf.multiply(pp, sample_weight)
+        tp = tf.reduce_sum(tf.cast(y_true_i & y_pred_i, self.dtype))
+        fp = tf.reduce_sum(tf.cast(~y_true_i & y_pred_i, self.dtype))
 
-        self.true_positives.assign_add(tf.reduce_sum(tp, axis=0))
-        self.pred_positives.assign_add(tf.reduce_sum(pp, axis=0))
-
-    def reset_state(self):
-        self.true_positives.assign(tf.zeros(shape=self.num_classes))
-        self.pred_positives.assign(tf.zeros(shape=self.num_classes))
+        self.tp.assign_add(tp)
+        self.fp.assign_add(fp)
 
     def result(self):
-        result = tf.math.divide_no_nan(self.true_positives, self.pred_positives)
-        return result
+        return self.tp / (self.tp + self.fp + tf.keras.backend.epsilon())
+
+    def reset_state(self):
+        self.tp.assign(0.0)
+        self.fp.assign(0.0)
 
 
-class Recall_per_class(tf.keras.metrics.Metric):
-    def __init__(self, name="Recall_per_class", num_classes=4, **kwargs):
-        super(Recall_per_class, self).__init__(name=name, **kwargs)
-        self.true_positives = self.add_weight(
-            name="tp", initializer="zeros", shape=num_classes
-        )
-        self.positives = self.add_weight(
-            name="positives", initializer="zeros", shape=num_classes
-        )
-        self.num_classes = num_classes
+class RecallForClass(tf.keras.metrics.Metric):
+    def __init__(self, class_id, name=None, **kwargs):
+        if name is None:
+            name = f"recall_class{class_id}"
+        super().__init__(name=name, **kwargs)
+        self.class_id = class_id
+        self.tp = self.add_weight(name="tp", initializer="zeros")
+        self.fn = self.add_weight(name="fn", initializer="zeros")
 
     def update_state(self, y_true, y_pred, sample_weight=None):
-        y_pred = tf.math.argmax(y_pred, axis=-1)
-        y_pred = tf.one_hot(y_pred, self.num_classes)
+        if y_true.shape.rank > 1 and y_true.shape[-1] > 1:
+            y_true = tf.argmax(y_true, axis=-1)
+        y_pred = tf.argmax(y_pred, axis=-1)
 
-        y_true = tf.cast(y_true, tf.bool)
-        y_pred = tf.cast(y_pred, tf.bool)
+        y_true = tf.cast(y_true, tf.int32)
+        y_pred = tf.cast(y_pred, tf.int32)
 
-        tp = tf.logical_and(tf.equal(y_true, True), tf.equal(y_pred, True))
-        tp = tf.cast(tp, self.dtype)
+        y_true_i = tf.equal(y_true, self.class_id)
+        y_pred_i = tf.equal(y_pred, self.class_id)
 
-        p = tf.equal(y_true, True)
-        p = tf.cast(p, self.dtype)
+        tp = tf.reduce_sum(tf.cast(y_true_i & y_pred_i, self.dtype))
+        fn = tf.reduce_sum(tf.cast(y_true_i & ~y_pred_i, self.dtype))
 
-        if sample_weight is not None:
-            sample_weight = tf.cast(sample_weight, self.dtype)
-            sample_weight = tf.broadcast_to(sample_weight, self.shape)
-            tp = tf.multiply(tp, sample_weight)
-            p = tf.multiply(p, sample_weight)
-
-        self.true_positives.assign_add(tf.reduce_sum(tp, axis=0))
-        self.positives.assign_add(tf.reduce_sum(p, axis=0))
-
-    def reset_state(self):
-        self.true_positives.assign(tf.zeros(shape=self.num_classes))
-        self.positives.assign(tf.zeros(shape=self.num_classes))
+        self.tp.assign_add(tp)
+        self.fn.assign_add(fn)
 
     def result(self):
-        result = tf.math.divide_no_nan(self.true_positives, self.positives)
+        return self.tp / (self.tp + self.fn + tf.keras.backend.epsilon())
 
-        return result
+    def reset_state(self):
+        self.tp.assign(0.0)
+        self.fn.assign(0.0)
+
+
+class SpecificityForClass(tf.keras.metrics.Metric):
+    def __init__(self, class_id, name=None, **kwargs):
+        if name is None:
+            name = f"specificity_class{class_id}"
+        super().__init__(name=name, **kwargs)
+        self.class_id = class_id
+        self.tn = self.add_weight(name="tn", initializer="zeros")
+        self.fp = self.add_weight(name="fp", initializer="zeros")
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        # Convert one-hot → integers if needed
+        if y_true.shape.rank > 1 and y_true.shape[-1] > 1:
+            y_true = tf.argmax(y_true, axis=-1)
+        y_pred = tf.argmax(y_pred, axis=-1)
+
+        y_true = tf.cast(y_true, tf.int32)
+        y_pred = tf.cast(y_pred, tf.int32)
+
+        # "Not class_id" mask
+        y_true_not_i = tf.not_equal(y_true, self.class_id)
+        y_pred_not_i = tf.not_equal(y_pred, self.class_id)
+
+        tn = tf.reduce_sum(tf.cast(y_true_not_i & y_pred_not_i, self.dtype))
+        fp = tf.reduce_sum(tf.cast(y_true_not_i & ~y_pred_not_i, self.dtype))
+
+        self.tn.assign_add(tn)
+        self.fp.assign_add(fp)
+
+    def result(self):
+        return self.tn / (self.tn + self.fp + tf.keras.backend.epsilon())
+
+    def reset_state(self):
+        self.tn.assign(0.0)
+        self.fp.assign(0.0)
